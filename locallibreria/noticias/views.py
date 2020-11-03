6c8b27ba0459e180fc33dc4d2b10fda72bd7508a
Mauricio <mauroyanezalarcon@gmail.com>
2020-11-03 18:31:21 -0300

    Locallibreria: arreglando view

--- locallibreria/noticias/views.py
+++ locallibreria/noticias/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 from .models import formulario
 from django.views import generic
 # Create your views here.
+def index (request):
+    num_Formu = formulario.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'nun_formu': num_Formu},
+    )
