1b2a618a0a43cd6b5e5bac4d8b942d1fb790a14e
Mauricio <mauroyanezalarcon@gmail.com>
2020-11-03 00:33:20 -0300

    Locallibreria: Creando modelos y el formulario

--- locallibreria/noticias/models.py
+++ locallibreria/noticias/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.urls import reverse  		#redirecciona una url de un libro al browser 
+import uuid  							#se utiliza para definir atributos clave (pk)
 # Create your models here.
+
+class Genre(models.Model):
+    #Model representing a book genre."""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class formulario(models.Model):
+    Nombre = models.CharField(max_length=30)
+    Apellido = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    Telefono = models.IntegerField()
+    Mensaje = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.Nombre
--- locallibreria/noticias/views.py
+++ locallibreria/noticias/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import formulario
+from django.views import generic
 # Create your views here.
+
