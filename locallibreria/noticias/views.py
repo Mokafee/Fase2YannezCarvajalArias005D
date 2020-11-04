b56cccd30a9c110dc6bad1abf1d43fbe062020ed
Mauricio <mauroyanezalarcon@gmail.com>
2020-11-03 21:42:55 -0300

    Locallibreria: error

--- locallibreria/noticias/views.py
+++ locallibreria/noticias/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import formulario
 from django.views import generic
 # Create your views here.
+
+from django.generic_edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+
 def contacto (request):
     num_Formu = formulario.objects.all().count()
 
